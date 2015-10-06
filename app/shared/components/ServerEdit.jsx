603cc08d57139e761e3571503727a376d659e606
AlejandroSilva <pm5k.sk@gmail.com>
2015-10-06 10:56:45 -0300

    FormGroup component

--- app/shared/components/ServerEdit.jsx
+++ app/shared/components/ServerEdit.jsx
@@ -8,7 +8,7 @@ import { connect } from 'react-redux'
 import * as ServersActions from '../actions/serversActions.js'
 
 // Components
-import { Alert } from './index.js'
+import { Alert, FormGroup } from './index.js'
 
 @connect(
     (state)=> ({
@@ -60,54 +60,43 @@ class ServerEdit extends React.Component {
                 </div>
                 <form className="form-horizontal" onSubmit={this.editForm.bind(this)}>
                     <div className="box-body">
-                        <div className="form-group">
-                            <label htmlFor="inputEmail3" className="col-sm-2 control-label">ID</label>
-                            <div className="col-sm-10">
-                                <input type="text" className="form-control"
-                                       value={this.state.server.id}
-                                       disabled/>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword3" className="col-sm-2 control-label">Nombre</label>
-                            <div className="col-sm-10">
-                                <input type="text" className="form-control"
-                                       defaultValue={this.state.server.name}
-                                       onChange={this.handleChange.bind(this, 'name')}
-                                       required placeholder="Nombre. Ej. CAO Biopacs"
+                        <FormGroup label="ID">
+                            <input type="text" className="form-control"
+                                   value={this.state.server.id}
+                                   disabled
                                 />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword3" className="col-sm-2 control-label">Host</label>
-                            <div className="col-sm-10">
-                                <input type="text" className="form-control"
-                                       defaultValue={this.state.server.host}
-                                       onChange={this.handleChange.bind(this, 'host')}
-                                       required placeholder="Host. Ej. cao.biopacs.com"
+                        </FormGroup>
+                        <FormGroup label="Nombre">
+                            <input type="text" className="form-control"
+                                   defaultValue={this.state.server.name}
+                                   onChange={this.handleChange.bind(this, 'name')}
+                                   placeholder="Nombre. Ej. CAO Biopacs"
+                                   required
                                 />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword3" className="col-sm-2 control-label">Port</label>
-                            <div className="col-sm-10">
-                                <input type="number" className="form-control"
-                                       defaultValue={this.state.server.port}
-                                       onChange={this.handleChange.bind(this, 'port')}
-                                       min="0" max="9999" required placeholder="Port. Ej. 8081"
+                        </FormGroup>
+                        <FormGroup label="Host">
+                            <input type="text" className="form-control"
+                                   defaultValue={this.state.server.host}
+                                   onChange={this.handleChange.bind(this, 'host')}
+                                   placeholder="Host. Ej. cao.biopacs.com"
+                                   required
+                            />
+                        </FormGroup>
+                        <FormGroup label="Puerto">
+                            <input type="number" className="form-control"
+                                   defaultValue={this.state.server.port}
+                                   onChange={this.handleChange.bind(this, 'port')}
+                                   min="0" max="9999" required placeholder="Port. Ej. 8081"
                                 />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword3" className="col-sm-2 control-label">Proyecto</label>
-                            <div className="col-sm-10">
-                                <input type="text" className="form-control"
-                                       defaultValue={this.state.server.project}
-                                       onChange={this.handleChange.bind(this, 'project')}
-                                       required placeholder="Proyecto. Ej. Clinica Alemana Osorno"
+                        </FormGroup>
+                        <FormGroup label="Proyecto">
+                            <input type="text" className="form-control"
+                                   defaultValue={this.state.server.project}
+                                   onChange={this.handleChange.bind(this, 'project')}
+                                   placeholder="Proyecto. Ej. Clinica Alemana Osorno"
+                                   required
                                 />
-                            </div>
-                        </div>
+                        </FormGroup>
                         <div className="col-sm-offset-2 col-sm-10">
                             <Alert title="Ocurrio un problema" message={this.state.error} show={this.state.error!==''}/>
                         </div>
